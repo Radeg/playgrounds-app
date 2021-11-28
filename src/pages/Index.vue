f94057d2895eca8bf3f7c1dd5dda4894cecbcc8a
Radeg <radekbenes@centrum.cz>
2021-11-28 11:17:40 +0100

    editorconfig

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,62 +1,66 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card class="q-ma-sm" v-for="item in data_playgroundsList" :key="item.id">
-      <img :src="item.properties.image.url" />
+	<q-page class="flex flex-center">
+		<q-card
+			class="q-ma-sm"
+			v-for="item in data_playgroundsList"
+			:key="item.id"
+		>
+			<img :src="item.properties.image.url" />
 
-      <q-card-section>
-        <q-btn
-          fab
-          color="primary"
-          icon="place"
-          class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%)"
-        />
-        <div class="text-h6">
-          {{ item.properties.name }}
-        </div>
-        <div class="text-subtitle2">{{ item.properties.perex }}</div>
-      </q-card-section>
+			<q-card-section>
+				<q-btn
+					fab
+					color="primary"
+					icon="place"
+					class="absolute"
+					style="top: 0; right: 12px; transform: translateY(-50%)"
+				/>
+				<div class="text-h6">
+					{{ item.properties.name }}
+				</div>
+				<div class="text-subtitle2">{{ item.properties.perex }}</div>
+			</q-card-section>
 
-      <q-card-section class="q-pt-none">
-        {{ item.properties.content }}
-      </q-card-section>
+			<q-card-section class="q-pt-none">
+				{{ item.properties.content }}
+			</q-card-section>
 
-      <q-card-section>
-        <iframe
-          width="450"
-          height="250"
-          frameborder="0"
-          style="border: 0"
-          src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyByMQs_LiFzIu_tTNcKnkTUhMysu3W14V0&&origin=Oslo+Norway&destination=Telemark+Norway&avoid=tolls|highways"
-          allowfullscreen
-        >
-        </iframe>
-      </q-card-section>
+			<q-card-section>
+				<iframe
+					width="450"
+					height="250"
+					frameborder="0"
+					style="border: 0"
+					src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyByMQs_LiFzIu_tTNcKnkTUhMysu3W14V0&&origin=Oslo+Norway&destination=Telemark+Norway&avoid=tolls|highways"
+					allowfullscreen
+				>
+				</iframe>
+			</q-card-section>
 
-      <q-separator />
+			<q-separator />
 
-      <q-card-actions>
-        <div class="text-caption">
-          <q-icon name="place" />
-          {{
-            getDestination(
-              data_currentLat,
-              data_currentLong,
-              item.geometry.coordinates[1],
-              item.geometry.coordinates[0]
-            ).toFixed(1)
-          }}
-          km
-        </div>
-      </q-card-actions>
-    </q-card>
-  </q-page>
-  <q-inner-loading
-    :showing="data_visible"
-    label="Please wait..."
-    label-class="text-teal"
-    label-style="font-size: 1.1em"
-  />
+			<q-card-actions>
+				<div class="text-caption">
+					<q-icon name="place" />
+					{{
+						getDestination(
+							data_currentLat,
+							data_currentLong,
+							item.geometry.coordinates[1],
+							item.geometry.coordinates[0]
+						).toFixed(1)
+					}}
+					km
+				</div>
+			</q-card-actions>
+		</q-card>
+	</q-page>
+	<q-inner-loading
+		:showing="data_visible"
+		label="Please wait..."
+		label-class="text-teal"
+		label-style="font-size: 1.1em"
+	/>
 </template>
 
 <script>
@@ -64,60 +68,60 @@ import { defineComponent } from "vue";
 import axios from "axios";
 
 export default defineComponent({
-  name: "PageIndex",
-  data() {
-    return {
-      data_playgroundsList: [],
-      data_currentLat: "",
-      data_currentLong: "",
-      data_visible: true,
-    };
-  },
-  methods: {
-    getPlaygroundList() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((pos) => {
-          this.data_currentLat = pos.coords.latitude;
-          this.data_currentLong = pos.coords.longitude;
-          let currentCoordinates = `${pos.coords.latitude}, ${pos.coords.longitude}`;
-          let options = {
-            headers: {
-              "x-access-token":
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZGVnMDA3QGdtYWlsLmNvbSIsImlkIjo4ODUsIm5hbWUiOm51bGwsInN1cm5hbWUiOm51bGwsImlhdCI6MTYyOTcwMDU3NSwiZXhwIjoxMTYyOTcwMDU3NSwiaXNzIjoiZ29sZW1pbyIsImp0aSI6ImVlNTI5ZDZmLTFmMDEtNGY5Mi1hN2U0LTIyY2E4OTAxMGM5NyJ9.ipb8SB59mWX_-HCS5Xh1I6Fmi8rypz7XV2a90uiWyM0",
-            },
-            params: { latlng: currentCoordinates, limit: "10" },
-          };
+	name: "PageIndex",
+	data() {
+		return {
+			data_playgroundsList: [],
+			data_currentLat: "",
+			data_currentLong: "",
+			data_visible: true,
+		};
+	},
+	methods: {
+		getPlaygroundList() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition((pos) => {
+					this.data_currentLat = pos.coords.latitude;
+					this.data_currentLong = pos.coords.longitude;
+					let currentCoordinates = `${pos.coords.latitude}, ${pos.coords.longitude}`;
+					let options = {
+						headers: {
+							"x-access-token":
+								"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZGVnMDA3QGdtYWlsLmNvbSIsImlkIjo4ODUsIm5hbWUiOm51bGwsInN1cm5hbWUiOm51bGwsImlhdCI6MTYyOTcwMDU3NSwiZXhwIjoxMTYyOTcwMDU3NSwiaXNzIjoiZ29sZW1pbyIsImp0aSI6ImVlNTI5ZDZmLTFmMDEtNGY5Mi1hN2U0LTIyY2E4OTAxMGM5NyJ9.ipb8SB59mWX_-HCS5Xh1I6Fmi8rypz7XV2a90uiWyM0",
+						},
+						params: { latlng: currentCoordinates, limit: "10" },
+					};
 
-          axios
-            .get("https://api.golemio.cz/v2/playgrounds/", options)
-            .then((res) => {
-              this.data_visible = false;
-              console.log(res.data.features);
-              this.data_playgroundsList = res.data.features;
-            });
-        });
-      }
-    },
-    getDestination(lat1, lon1, lat2, lon2) {
-      var R = 6371; // Radius of the earth in km
-      var dLat = this.deg2rad(lat2 - lat1); // deg2rad below
-      var dLon = this.deg2rad(lon2 - lon1);
-      var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(this.deg2rad(lat1)) *
-          Math.cos(this.deg2rad(lat2)) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      var d = R * c; // Distance in km
-      return d;
-    },
-    deg2rad(deg) {
-      return deg * (Math.PI / 180);
-    },
-  },
-  mounted() {
-    this.getPlaygroundList();
-  },
+					axios
+						.get("https://api.golemio.cz/v2/playgrounds/", options)
+						.then((res) => {
+							this.data_visible = false;
+							console.log(res.data.features);
+							this.data_playgroundsList = res.data.features;
+						});
+				});
+			}
+		},
+		getDestination(lat1, lon1, lat2, lon2) {
+			var R = 6371; // Radius of the earth in km
+			var dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+			var dLon = this.deg2rad(lon2 - lon1);
+			var a =
+				Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+				Math.cos(this.deg2rad(lat1)) *
+					Math.cos(this.deg2rad(lat2)) *
+					Math.sin(dLon / 2) *
+					Math.sin(dLon / 2);
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			var d = R * c; // Distance in km
+			return d;
+		},
+		deg2rad(deg) {
+			return deg * (Math.PI / 180);
+		},
+	},
+	mounted() {
+		this.getPlaygroundList();
+	},
 });
 </script>
