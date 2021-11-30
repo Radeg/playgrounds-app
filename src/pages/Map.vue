47d02537ca512e4842285b865283f72702c6adf0
Radeg <radekbenes@centrum.cz>
2021-11-30 14:56:02 +0100

    fixed map height

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -5,9 +5,12 @@
 			height="100%"
 			frameborder="0"
 			style="border: 0"
-			:src="'https://www.google.com/maps/embed/v1/place?key=AIzaSyByMQs_LiFzIu_tTNcKnkTUhMysu3W14V0&&q=' + data_coordinates"
+			:src="
+				'https://www.google.com/maps/embed/v1/place?key=AIzaSyByMQs_LiFzIu_tTNcKnkTUhMysu3W14V0&&q=' +
+				data_coordinates
+			"
 			allowfullscreen
-				>
+		>
 		</iframe>
 	</q-page>
 </template>
@@ -19,17 +22,17 @@ export default defineComponent({
 	name: "Map",
 	data() {
 		return {
-			data_coordinates: ""
-		}
+			data_coordinates: "",
+		};
 	},
 	created() {
 		this.data_coordinates = this.$route.params.loc;
-	}
+	},
 });
 </script>
 
 <style scoped>
 iframe {
-	height: 100vh;
+	height: 93vh;
 }
 </style>
